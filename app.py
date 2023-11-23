4dcb6636b6e7d70fdbf292ebaca8569b084fe985
Pratik-sys <pithore.pratik@gmail.com>
2023-11-23 21:17:51 +0530

    Use Jsonify

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os, json
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restx import Api, Resource
 from mongoengine import connect
 from dotenv import load_dotenv
@@ -12,31 +12,20 @@ load_dotenv(".env")
 connect(host=os.getenv("MONGO_URI"))
 
 
-@api.route("/test")
-class TestAPI(Resource):
-    """This API just for testing purpose"""
-
-    def get(self):
-        """Get req to check whether api returns the defined response"""
-        return {"msg": "Testing API"}
-
-
 @api.route("/AddQ")
 class AddQuotes(Resource):
-    """This API just for testing purpose"""
-
     def post(self):
-        """Post req to add in DB"""
+        """Post req to add details in Database"""
         record = json.loads(request.data)
         try:
             if len(record) != 0:
                 quote = Quotes(title=record["title"], author=record["author"])
                 quote.save()
-                return {"msg":"data is dumped"}
+                return jsonify({"Msg": "data is dumped"}, 200)
             else:
-                return {"msg": "Fields are empty"}
-        except:
-            return {"msg": "something is wrong"}
+                return jsonify({"Msg": "Fields are empty"}, 201)
+        except Exception as ex:
+            return jsonify({"Msg": ex}, 404)
 
 
 if __name__ == "__main__":
