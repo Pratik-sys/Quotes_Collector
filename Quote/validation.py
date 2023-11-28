de891d2bd8387d68096f81ee54ad7b2e53829fa2
Pratik-sys <pithore.pratik@gmail.com>
2023-11-28 17:29:24 +0530

    Fix camel casing

--- Quote/validation.py
+++ Quote/validation.py
@@ -2,14 +2,16 @@ from Quote.models import Quotes
 from Quote.extension import app
 
 
-def validateQuotes(quote):
+def validate_quotes(quote):
+    """function will validate the data coming from JSON"""
     errors = []
     if quote.title == "":
         errors.append("Title field cannot be empty")
         app.logger.warning("Title field is empty, return the error array")
     return errors
 
-def validateUpdateQuotes(record):
+def validate_update_quotes(record):
+    """function will check for the fileds while updating in database"""
     errors = []
     if record["title"] == "":
         errors.append("Cannot update title as blank field")
